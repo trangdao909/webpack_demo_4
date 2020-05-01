b5549fb0c629ec040aa845117f8209b80d58dbc9
Trang Dao <trang.dao909@tcu.edu>
2020-04-30 23:35:07 -0500

    webpack_demo_4

--- webpack.config.js
+++ webpack.config.js
@@ -5,22 +5,31 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/index.js',
-    print: './src/print.js',
+    app: './src/index.js',   
   },
   devtool: 'inline-source-map',
   devServer: {
          contentBase: './dist',
+         hot: true,
        },
+  module: {
+        rules: [
+          {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
   plugins: [
     //new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    //new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Development',
+      title: 'Hot Module Replacement',
     }),
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+   // publicPath: '/',
   },
 };
